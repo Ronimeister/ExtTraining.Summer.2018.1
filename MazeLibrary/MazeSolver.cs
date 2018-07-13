2689b617c6253b3cbb7c2f9f308b2c8bd2094036
Ronimeister <ronimeisterhs@gmail.com>
2018-07-13 23:12:24 +0300

    Refactored. Comments added

--- MazeLibrary/MazeSolver.cs
+++ MazeLibrary/MazeSolver.cs
@@ -16,6 +16,14 @@ namespace MazeLibrary
         #endregion
 
         #region Public API
+        /// <summary>
+        /// Constructor for <see cref="MazeSolver"/> class
+        /// </summary>
+        /// <param name="mazeModel"><see cref="int[,]"/> map</param>
+        /// <param name="startX">Start position X-coord.</param>
+        /// <param name="startY">Start position Y-coord.</param>
+        /// <exception cref="ArgumentException">Throw when <paramref name="startX"/> or <paramref name="startY"/> is less than 0 or when start position is equal to -1</exception>
+        /// <exception cref="ArgumentNullException">Throws when <paramref name="mazeModel"/> is equal to null or one of indexes <paramref name="startX"/> or <paramref name="startY"/> is out of range</exception>
         public MazeSolver(int[,] mazeModel, int startX, int startY)
         {
             if (mazeModel is null)
@@ -23,7 +31,7 @@ namespace MazeLibrary
                 throw new ArgumentNullException($"{nameof(mazeModel)} can't be equal to null!");
             }
 
-            if(startX < 0 || startY < 0)
+            if (startX < 0 || startY < 0)
             {
                 throw new ArgumentException($"Index can't be less the 0!");
             }
@@ -45,8 +53,16 @@ namespace MazeLibrary
             _mapWidth = mazeModel.GetLength(1);
         }
 
+        /// <summary>
+        /// Method that return current state of map
+        /// </summary>
+        /// <returns>Current state of map</returns>
         public int[,] MazeWithPass() => _map;
 
+        /// <summary>
+        /// Method that finds the way from start point to the end point
+        /// </summary>
+        /// <exception cref="ArgumentException">Throw when <see cref="_map"/> hasn't got any exit points</exception>
         public void PassMaze()
         {
             if (!IsWithFinish())
@@ -62,18 +78,6 @@ namespace MazeLibrary
         #endregion
 
         #region Private API
-        public void Print()
-        {
-            for (int i = 0; i < _mapHeight; i++)
-            {
-                for (int j = 0; j < _mapWidth; j++)
-                {
-                    Console.Write(_map[i, j] + "  ");
-                }
-                Console.WriteLine();
-            }
-        }
-
         private bool IsWithFinish() => (CountFinishes(_map) == 2) ? true : false;
 
         private int CountFinishes(int[,] map)
@@ -97,7 +101,7 @@ namespace MazeLibrary
             return result;
         }
 
-        public (int, int) FindFinish(int[,] _map)
+        private (int, int) FindFinish(int[,] _map)
         {
             for (int i = 0; i < _mapHeight; i++)
             {
@@ -148,37 +152,21 @@ namespace MazeLibrary
 
                 if (incX < _mapHeight && _map[incX, y] == 0)
                 {
-                    if(_map[incX, y] == prev)
-                    {
-                        _map[x, y] = 0;
-                    }
                     CompleteTheMap(_map[x, y], incX, y, step);
                 }
 
                 if(incY < _mapWidth && _map[x, incY] == 0)
                 {
-                    if (_map[x, incY] == prev)
-                    {
-                        _map[x, y] = 0;
-                    }
                     CompleteTheMap(_map[x, y], x, incY, step);
                 }
 
                 if (decX >= 0 && _map[decX, y] == 0)
                 {
-                    if (_map[decX, y] == prev)
-                    {
-                        _map[x, y] = 0;
-                    }
                     CompleteTheMap(_map[x, y], decX, y, step);
                 }
 
                 if (decY >= 0 && _map[x, decY] == 0)
                 {
-                    if (_map[x, decY] == prev)
-                    {
-                        _map[x, y] = 0;
-                    }
                     CompleteTheMap(_map[x, y], x, decY, step);
                 }
 
